db382a47af83a35d21479052f4127b27da8776e4
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2025-01-04 20:28:31 -0300

    Add react-chapter-10 as a regular directory

---
+++ Chapter10/React/react-chapter-10/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div className="my-component">
+      <h2>This is my first component!</h2>
+      <p>Congratulations for your first component.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
