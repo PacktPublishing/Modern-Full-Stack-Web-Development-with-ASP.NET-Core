63b7269b36089b850cbc66fabd591e3437ef4bac
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2025-01-04 20:33:37 -0300

    Add chapter-10-vue-app as a regular directory

---
+++ Chapter10/Vue/chapter-10-vue-app/src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="my-component">
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyComponent',
+    data() {
+      return {
+        title: 'My first Vue component!',
+        description: 'I am proud of my first component.'
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .my-component {
+    border: 1px solid black;
+    padding: 15px;
+    border-radius: 4px;
+    background-color: blanchedalmond;
+  }
+  .my-component h2 {
+    color: black;
+  }
+  </style>
+  
