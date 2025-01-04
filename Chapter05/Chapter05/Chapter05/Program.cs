d26f7110dd5b33beb83d7b01f816d6b278ab0659
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2025-01-04 15:15:22 -0300

    Chapter 5

---
+++ Chapter05/Chapter05/Chapter05/Program.cs
@@ -0,0 +1,38 @@
+using Chapter05.Client.Pages;
+using Chapter05.Components;
+
+namespace Chapter05
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
