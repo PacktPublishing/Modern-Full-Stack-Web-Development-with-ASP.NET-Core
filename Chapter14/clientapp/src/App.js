4d9458791ae509c3e603471ab201b3189cdba3aa
Alexandre Felipe Malavasi Cardoso <alexandremalavasi@hotmail.com>
2025-01-04 22:53:39 -0300

    Add Chapter14/clientapp as a regular directory

---
+++ Chapter14/clientapp/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PollList from './components/PollList';
+import PollDetails from './components/PollDetails';
+import PollResults from './components/PollResults';
+import Home from './components/Home'; 
+import CreatePoll from './components/CreatePoll';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> {}
+        <Route path="/polls" element={<PollList />} /> {}
+        <Route path="/poll/:pollId" element={<PollDetails />} /> {}
+        <Route path="/poll/:pollId/results" element={<PollResults />} /> {}
+        <Route path="/create-poll" element={<CreatePoll />} /> {}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
